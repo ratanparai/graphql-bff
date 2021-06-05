f53c1fcb834c735cfa300f279b55146ba048482a
Ratan Sunder Parai <ratan.parai@selise.ch>
2021-06-05 16:39:39 +0600

    feat(student): add student endpoint

---
+++ src/Services/Student/Student.API/Controllers/StudentController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Mime;
+
+namespace Student.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    [Produces(MediaTypeNames.Application.Json)]
+    [Consumes(MediaTypeNames.Application.Json)]
+    public class StudentController
+        : ControllerBase
+    {
+        [HttpGet("{id}", Name = "GetStudent")]
+        [ProducesResponseType(typeof(StudentDto), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+        public ActionResult<StudentDto> GetStudent(string id)
+        {
+            if (id == "Null")
+            {
+                return NotFound();
+            }
+            return Ok(new StudentDto(id, "Ratan", 12));
+        }
+    }
+
+    public class StudentDto
+    {
+        public StudentDto(string id, string name, int age)
+        {
+            Id = id;
+            Name = name;
+            Age = age;
+        }
+
+        public string Id { get; }
+
+        public string Name { get; }
+
+        public int Age { get; }
+    }
+}
